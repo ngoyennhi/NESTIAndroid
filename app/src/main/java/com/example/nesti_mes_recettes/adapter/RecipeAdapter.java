3716ae524d6a86e9b79a89ee57b732ac64baf188
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-05-20 13:29:47 +0200

    mettre à jour l'adaptateur

--- app/src/main/java/com/example/nesti_mes_recettes/adapter/RecipeAdapter.java
+++ app/src/main/java/com/example/nesti_mes_recettes/adapter/RecipeAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -43,6 +44,8 @@ public class RecipeAdapter extends ArrayAdapter<Recipe> {
 
         TextView author = result.findViewById(R.id.lineRecipe_txtViewAuthor);
         author.setText(one_recipe.getAuthor());
+
+        ImageView imageView =result.findViewById(R.id.lineRecipe_imgViewRecipe);
 
         return result;
     }
