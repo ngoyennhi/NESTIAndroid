e7fecc385f28051cebd5608030602b447ae302ec
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-05-31 11:12:13 +0200

    Update GlutenActivity.java

--- app/src/main/java/com/example/nesti_mes_recettes/GlutenActivity.java
+++ app/src/main/java/com/example/nesti_mes_recettes/GlutenActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.view.View;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -18,11 +19,12 @@ import com.example.nesti_mes_recettes.entity.Recipe;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
+import org.json.JSONException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+
 
 public class GlutenActivity extends AppCompatActivity {
 
@@ -31,21 +33,21 @@ public class GlutenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gluten);
 
-        requestApi();
+        requestApi(this);
 
     }
 
-    private void requestApi(){
+    private void requestApi(GlutenActivity glutenActivity){
         //l’url qui sera interrogée
         //Récupérez l’url du end-point qui fournis les recettes de la catégories “Sans gluten”
         // localhost
-        String url = "http://localhost:8888/nestiADMIN_CodeIgniter4/project_root_CodeIgniter4/public/index.php/api/category/sansgluten";
+      String url = "http://localhost:8888/nestiADMIN_CodeIgniter4/project_root_CodeIgniter4/public/index.php/api/category/sansgluten";
 
        // // server
        // //Remplacer les pointillés par le nom du projet Codeigniter.
        // //Remplacer les ... par votre chemin. (normalement c’est le nom du projet)
-       // //Important : Remplacer “127.0.0.1” par votre adresse ip
-       // String url = "http://192.168.1.65/www/.../public/index.php/api/category/gluten"
+       // //Important : Remplacer “192.168.0.000” par ip mac
+       // String url = "http://192.168.0.000/www/.../public/index.php/api/category/gluten"
 
        final RequestQueue request_queue = Volley.newRequestQueue(getApplicationContext());
         JsonArrayRequest array_request = new JsonArrayRequest(
@@ -55,7 +57,12 @@ public class GlutenActivity extends AppCompatActivity {
                          @Override
                          public void onResponse(JSONArray response){
                             //Traitement de la réponse
-                            Log.i("LogNesti",response.toString());
+                             ArrayList<Recipe> gluten_activity = readJSONRecipe(response.toString());
+                             Log.i("LogNesti", "tableau reçu:" + gluten_activity.size());
+                             ListView list_view = (ListView) findViewById(R.id.sansGluten_listView);
+                             RecipeAdapter adapter = new RecipeAdapter(glutenActivity, R.layout.line_recipe, gluten_activity);
+                             list_view.setAdapter(adapter);
+                            //Log.i("LogNesti",response.toString());
                          }
                     },
                 //Interface de rappel pour fournir des réponses d'erreur.
@@ -71,15 +78,48 @@ public class GlutenActivity extends AppCompatActivity {
         );
         request_queue.add(array_request);
     }
+    /*
+   Ajouter une méthode qui, à partir d’une chaîne de caractère JSON,
+   donne un tableau d’objet de type Recipe.
+    */
+    private ArrayList<Recipe> readJSONRecipe(String response) {
+        ArrayList<Recipe> recipes = new ArrayList<>();
+        try{
+            JSONArray tableau_JSON = new JSONArray(response);
+            //classe JSON Array pour passer les données du fichier JSON
+            //note: il est impératif de stocker le fichier season.json dans ASSETS
 
-    /*
-    Ajouter une méthode qui, à partir d’une chaîne de caractère JSON,
-    donne un tableau d’objet de type Recipe.
-     */
-    /*
-    Ajouter une méthode qui, à partir d’une chaîne de caractère JSON,
-    donne un tableau d’objet de type Recipe.
-     */
+            Log.i("LogNesti","Nombre denregistrements : " + tableau_JSON.length());
+            //Parcours du tableau
+            for(int i = 0;i<tableau_JSON.length(); i++){
+                JSONObject object_JSON = tableau_JSON.getJSONObject(i);
+                //Une fois les données JSON du fichier seazon.json récupérées dans le tableau JSONArray,
+                // il suffira de parcourir ce tableau et d’extraire les informations concernant chacune des recettes de la catégorie seazon,
+                // grâce à la classe JSONObject.
+                Recipe r = new Recipe();
+                // chaque lot d'info sera stocké dans un objet Recipe
+                r.setCat(object_JSON.getString("cat"));
+                r.setTitle(object_JSON.getString("title"));
+                r.setAuthor(object_JSON.getString("author"));
+
+                int index = this.getResourceImage(object_JSON.getString("img"));
+                r.setImgId(index);
+
+                r.setDifficulty(object_JSON.getInt("diff"));
+                String imageDiff = difficultImageConverter(r.getDifficulty());
+                int iddiff = getResources().getIdentifier(imageDiff,"drawable", getPackageName());
+                r.setDifficulty(iddiff);
+
+                // A completer
+                recipes.add(r);
+            }
+        }
+        catch (Exception e){
+            Log.e("LogNesti","Erreur de lecture du Json");
+            e.printStackTrace();
+        }
+        return recipes;
+    }
 
     /*
      //Retourne l'identifiant d'une image
@@ -90,4 +130,26 @@ public class GlutenActivity extends AppCompatActivity {
         String path = getPackageName()+ ":drawable/" + nameImage;
         return getResources().getIdentifier(path,null,null);
     }
-}
+
+    private String difficultImageConverter(int diff) {
+        String img_name = new String();
+        if (diff == 1) {
+            img_name = "star_1";
+            Log.i("img_name", img_name);
+        } else if (diff == 2) {
+            img_name = "star_2";
+            Log.i("img_name", img_name);
+        } else if (diff == 3) {
+            img_name = "star_3";
+            Log.i("img_name", img_name);
+        } else if (diff == 4) {
+            img_name = "star_4";
+            Log.i("img_name", img_name);
+        } else if (diff == 5) {
+            img_name = "star_5";
+            Log.i("img_name", img_name);
+        }
+        return img_name;
+    }
+
+}//end classe
